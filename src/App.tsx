1619ae540a827bd30452d5f824dcab9a74b9b818
Jubiscreúbio <vinicius.matheus@estudante.ifms.edu.br>
2024-03-12 10:04:00 -0400

    Meu Primeiro Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css"
+import MeuComp from './componentes/MeuComp'
+let nome = "zz"
+let sobrenome = "zezio"
+function App() {
+  return (
+    <>
+    <MeuComp/>
+    <h1 className= "myname">{nome} {sobrenome}, ama kiq e pauladas</h1>
+    </>
+  )// JavaScript XML
+}
+
+export default App
